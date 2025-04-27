82ef699a41a1899541d4cb93df8d27c28d888f41
Ronen Gelyadov <ronengtv@gmail.com>
2025-04-27 11:57:29 +0300

    27.4.25 12:00

--- Assets/_Scripts/Interactables/GoldCoin.cs
+++ Assets/_Scripts/Interactables/GoldCoin.cs
@@ -1,13 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GoldCoin : MonoBehaviour
 {
     public void RandomPopUp() {
-        transform.position = transform.position + new Vector3(Random.Range(-2f,2f), -0.5f, 0);
+        transform.position = transform.position + new Vector3(Random.Range(-2f, 2f), -0.5f, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.GetComponent<PlayerController>()){
+        if (other.gameObject.GetComponent<PlayerController>()) {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        Debug.Log("Something");
+
+        if (true) {
             Destroy(gameObject);
         }
     }
